3b02a3e11c88fe2985abf7009ff3818295686d88
Michael Vaden <michaelvaden03@gmail.com>
2022-05-21 13:22:43 -0400

    started work on animation creation page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "928dceaaea530a52"; }
+/******/ 		__webpack_require__.h = function() { return "fd7dd6d92fcdbcf6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/pages/test.js
+++ frontend/pages/test.js
@@ -1,7 +1,15 @@
 import style from '../styles/Test.module.css'
+import { useRef, useEffect, useState} from 'react'
 
 
 export default function Test() {
+const [name, setName] = useState("circle");
+
+    // We need to spread the previous state and change the one we're targeting, so other data cannot be lost.
+    const handleChange = (value) => {
+        setName(value);
+    }
+
   return (
     <div style={{backgroundColor: '#1a1a1a', width: '100vw', height: '100vh', display: 'flex'}}>
         <div style={{ backgroundColor: '#1a1a1a', width: '15vw', height: '100vh', borderRight: '1px solid #f8edd6'}}>
@@ -26,12 +34,39 @@ export default function Test() {
         </div>
         <div style={{ backgroundColor: '#1a1a1a', width: '15vw', height: '100vh', borderRight: '1px solid #f8edd6'}}>
             <div className={style.testTitle}>
-                <input type="text" placeholder="circle-0" />
-                circle-0
+                <input
+                    className={style.testInput}
+                    type="text"
+                    value={name}
+                    onChange={(e) => handleChange(e.target.value)}
+                    spellcheck="false"
+                />
             </div>
             <div className={style.testSubtitle}>
                 Circle
             </div>
+            <div className={style.testGrid}>
+                <div>
+                    <label style={{color: '#ffffff'}} htmlFor="x" >x</label>
+                    <input style={{width: '45px'}} type="text" name="x" id="x"/>
+                </div>
+                
+                <div>
+                    <label style={{color: '#ffffff'}} htmlFor="y" >y</label>
+                    <input style={{width: '45px'}} type="text" name="y" id="y"/>
+                </div>
+                
+                <div>
+                    <label style={{color: '#ffffff'}} htmlFor="w" >w</label>
+                    <input style={{width: '45px'}} type="text" name="w" id="w"/>
+                </div>
+
+                <div>
+                    <label style={{color: '#ffffff'}} htmlFor="h" >h</label>
+                    <input style={{width: '45px'}} type="text" name="h" id="h"/>
+                </div>
+            </div>
+
         </div>
         <div style={{ backgroundColor: '#1a1a1a', width: '70vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <div style={{ backgroundColor: '#9CC4B2', width: '950px', height: '700px', borderRadius: '2rem'}}>
