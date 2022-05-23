cf0e3cff0e9cb4ea59882c378801737202de876d
Michael Vaden <michaelvaden03@gmail.com>
2022-05-23 18:57:56 -0400

    created new react app with ts

---
+++ oldfrontend/.next/server/pages/figureCreator.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/figureCreator";
+exports.ids = ["pages/figureCreator"];
+exports.modules = {
+
+/***/ "./pages/figureCreator.tsx":
+/*!*********************************!*\
+  !*** ./pages/figureCreator.tsx ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"FigureCreator\": () => (/* binding */ FigureCreator)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);\n\n // we need this to make JSX compile\nconst FigureCreator = ({ title , paragraph  })=>/*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"aside\", {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h2\", {\n                children: title\n            }, void 0, false, {\n                fileName: \"/home/michael/code/Atomic/frontend/pages/figureCreator.tsx\",\n                lineNumber: 9,\n                columnNumber: 3\n            }, undefined),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"p\", {\n                children: paragraph\n            }, void 0, false, {\n                fileName: \"/home/michael/code/Atomic/frontend/pages/figureCreator.tsx\",\n                lineNumber: 10,\n                columnNumber: 3\n            }, undefined)\n        ]\n    }, void 0, true, {\n        fileName: \"/home/michael/code/Atomic/frontend/pages/figureCreator.tsx\",\n        lineNumber: 8,\n        columnNumber: 67\n    }, undefined)\n;\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9maWd1cmVDcmVhdG9yLnRzeC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBeUIsQ0FBRSxFQUFtQztBQU92RCxLQUFLLENBQUNDLGFBQWEsSUFBSSxDQUFDLENBQUNDLEtBQUssR0FBRUMsU0FBUyxFQUFZLENBQUMsK0VBQU1DLENBQUs7O3dGQUNyRUMsQ0FBRTswQkFBR0gsS0FBSzs7Ozs7O3dGQUNWSSxDQUFDOzBCQUNFSCxTQUFTIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZnJvbnRlbmQvLi9wYWdlcy9maWd1cmVDcmVhdG9yLnRzeD9lZGFkIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCc7IC8vIHdlIG5lZWQgdGhpcyB0byBtYWtlIEpTWCBjb21waWxlXG5cbnR5cGUgQ2FyZFByb3BzID0ge1xuICB0aXRsZTogc3RyaW5nLFxuICBwYXJhZ3JhcGg6IHN0cmluZ1xufVxuXG5leHBvcnQgY29uc3QgRmlndXJlQ3JlYXRvciA9ICh7IHRpdGxlLCBwYXJhZ3JhcGggfTogQ2FyZFByb3BzKSA9PiA8YXNpZGU+XG4gIDxoMj57IHRpdGxlIH08L2gyPlxuICA8cD5cbiAgICB7IHBhcmFncmFwaCB9XG4gIDwvcD5cbjwvYXNpZGU+XG4iXSwibmFtZXMiOlsiUmVhY3QiLCJGaWd1cmVDcmVhdG9yIiwidGl0bGUiLCJwYXJhZ3JhcGgiLCJhc2lkZSIsImgyIiwicCJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/figureCreator.tsx\n");
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/figureCreator.tsx"));
+module.exports = __webpack_exports__;
+
+})();
