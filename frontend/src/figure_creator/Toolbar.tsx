70a5c54949d5c12445a9ca515d1ed7efbe31ac2f
Michael Vaden <michaelvaden03@gmail.com>
2022-05-25 19:14:56 -0400

    started work on inputs in toolbar

--- frontend/src/figure_creator/Toolbar.tsx
+++ frontend/src/figure_creator/Toolbar.tsx
@@ -1,6 +1,8 @@
 import React, {FC, useState, ChangeEvent} from 'react'
 import style from '../styles/FigureCreator.module.css'
 
+import TextField from './TextField'
+
 interface Props {
     width: number;
     height: number;
@@ -8,10 +10,22 @@ interface Props {
 }
 
 export const Toolbar: FC<Props> = ({width, height, backgroundColor}) => {
+    const [name, setName] = useState<string>("circle");
+
+    const handleChange = (value: string) => {
+        setName(value);
+    }
 
     return (
         <div className={style.toolBar} style={{width: `${width}px`, backgroundColor: backgroundColor}}>
-            
+            <input
+                className={style.testInput}
+                type="text"
+                value={name}
+                onChange={(e) => handleChange(e.target.value)}
+                spellCheck="false"
+            />
+            <TextField name="x" color='#f8edd6' backgroundColor='#332F2E' />
         </div>
     )
 }
