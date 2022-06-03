5ca621f358b2ef8504db2976ad54b813c4004753
Michael Vaden <michaelvaden03@gmail.com>
2022-06-02 20:19:00 -0400

    updating project to redux, need to learn about this

--- frontend/src/canvas/CanvasPage.jsx
+++ frontend/src/canvas/CanvasPage.jsx
@@ -8,17 +8,47 @@ import { Section } from '../canvas/toolbar/Section';
 import { Surface } from './objects/Surface'
 
 // actions for reducer
-const Action = {
+export const Action = {
     EDIT: Symbol('EDIT'),
+    TOGGLESELECTED: Symbol('TOGGLESELECTED'),
 }
 
 // state-reducer
+const getAttr = (state, keys, val) => {
+    let stateHistory = [state];
+
+   for ( let i = 0; i < keys.length; i++) {
+        const recursiveState = stateHistory[i][keys[i]];
+
+        if (typeof recursiveState === 'object') {
+            stateHistory.push(recursiveState)
+        } else {
+            return recursiveState;
+        }
+   }
+};
+
 const canvasReducer = (state, action) => {
     const { type, payload } = action;
 
     switch (type) {
         case Action.EDIT:
-            state.payload.attr = payload.value;
+            const keys = payload.keys;
+            const val = payload.value;
+
+            if (keys.length == 0) {
+                state = state;
+            } else
+            if (keys.length == 1) {
+                state[keys[0]] = val;
+            } else
+            if (keys.length == 2) {
+                state[keys[0]][keys[1]] = val;
+            }
+
+            return state
+        case Action.TOGGLESELECTED:
+            state.global.objectSelected = payload.value;
 
             return state
         default:
@@ -35,24 +65,33 @@ export const CanvasPage = ({backgroundColor}) => {
     const [canvasContext, canvasDispatch] = useReducer(canvasReducer, 
         {
             global: {
-                objectSelected: false,
+                objectSelected: '',
+                resizeOffset: 5
             },
             page: {
+                tag: 'page',
+                resizing: false,
                 width: window.innerWidth,
                 height: window.innerHeight,
                 backgroundColor: '#1a1a1a',
             },
             toolbar: {
+                tag: 'toolbar',
+                resizing: false,
                 width: 220,
                 height: window.innerHeight,
                 backgroundColor: '#1a1a1a',
                 color: '#fff0ce',
             },
             surface: {
+                tag: 'surface',
+                resizing: false,
                 top: (window.innerHeight - 800) / 2,
                 left: (window.innerWidth - 220 - 800) / 2,
                 width: 800,
                 height: 800,
+                minW: 100,
+                minH: 100,
                 backgroundColor: '#1a1a1a',
                 foregroundColor: '#383838',
                 selected: false,
--- frontend/src/canvas/objects/Surface.jsx
+++ frontend/src/canvas/objects/Surface.jsx
@@ -1,24 +1,55 @@
 import React, { useCallback, useEffect } from 'react'
 import style from '../../styles/canvas/Surface.module.css'
 
-import { useCanvasContext } from '../CanvasPage'
+import { useCanvasContext, Action } from '../CanvasPage'
 
 export const Surface = ({children}) => {
     const [canvasContext, canvasDispatch] = useCanvasContext();
 
     const handleMouseMove = useCallback((event) => {
+        // mouse position
         const x = event.pageX;
         const y = event.pageY;
 
-        const t = canvasContext.surface.top;
-        const l = canvasContext.surface.left + canvasContext.toolbar.width;
-        const w = canvasContext.surface.width;
-        const h = canvasContext.surface.height;
+        // positioning
+        const t = canvasContext['surface']['top'];
+        const l = canvasContext['surface']['left'] + canvasContext['toolbar']['width'];
+        const b = t + canvasContext['surface']['height'];
+        const r = l + canvasContext['surface']['width'];
 
-        if (x < l) {
-            alert("asdf")
-        }
-    }, [])
+        // offset
+        const o = canvasContext['global']['resizeOffset'];
+
+        if (x > l - o && x < l + o && y > t - o && y < t + o) {     // top left
+            document.body.style.cursor = 'nwse-resize';
+        } else
+        if (x > r - o && x < r + o && y > b - o && y < b + o) {     // bottom right
+            document.body.style.cursor = 'nwse-resize';
+        } else
+        if (x > r - o && x < r + o && y > t - o && y < t + o) {     // top right
+            document.body.style.cursor = 'nesw-resize';
+        } else
+        if (x > l - o && x < l + o && y > b - o && y < b + o) {     // bottom left
+            document.body.style.cursor = 'nesw-resize';
+        } else
+        if (x > l + o && x < r - o && y > t - o && y < t + o) {     // top
+            document.body.style.cursor = 'ns-resize';
+        } else
+        if (x > l - o && x < l + o && y > t + o && y < b - o) {     // left
+            document.body.style.cursor = 'ew-resize';
+        } else
+        if (x > l + o && x < r - o && y > b - o && y < b + o) {     // bottom
+            document.body.style.cursor = 'ns-resize';
+        } else
+        if (x > r - o && x < r + o && y > t + o && y < b - o) {     // left
+            document.body.style.cursor = 'ew-resize';
+        } else {
+            document.body.style.cursor = 'alias';
+        };
+
+        canvasDispatch({type: Action.EDIT, payload: {keys: ['surface', 'width'], value: 400}})
+
+    }, [canvasContext])
 
     useEffect(() => {
         document.addEventListener('mousemove', handleMouseMove);
