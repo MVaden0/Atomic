f1178ac5921caaeca0bc572a3f60773596c798b2
Michael Vaden <michaelvaden03@gmail.com>
2022-06-03 14:40:17 -0400

    got rid of unneccessary files, pure django now

--- web/App/Hammer/main.py
+++
@@ -1,4 +0,0 @@
-from templates import get_file
-
-
-print(get_file("test.html"))
--- web/App/src/components/CustomButton/CustomButton.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('.custom-button');
-
-button.addEventListener('click', () => {
-    alert("My Button was clicked");
-})
