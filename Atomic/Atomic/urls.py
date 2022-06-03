8784975b1d0db68643a2ff208979ffc2ed277ac5
Michael Vaden <michaelvaden03@gmail.com>
2022-06-03 15:12:27 -0400

    added core and canvas to settings apps

--- Atomic/Atomic/urls.py
+++ Atomic/Atomic/urls.py
@@ -7,3 +7,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index")
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html", authentication_form=UserLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('workshops/', include('workshops.urls')),
+    path('surveys/', include('surveys.urls')),
+    path('', include('core.urls')),
+    path('registration/', include('registration.urls'))
+]
